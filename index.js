71cf65d515ec2dded8f89f4b84e89bf2f39fb0de
dimi3ryi <feschenko.dmitryi@gmail.com>
2017-07-16 22:40:21 +0300

    add support for pinned versions, downloading valid packages from Yarn repository

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 import fetch from 'node-fetch';
+import semver from 'semver';
+import fs from 'fs-extra';
 
 /**
  * Downloads package from Internet
- * @param reference
+ * @param   name
+ * @param   reference
  * @returns {Promise|*}
  */
-async function fetchPackage(reference) {
+async function fetchPackage({ name, reference }) {
+    // checks if a given reference is a valid version
+    if (semver.valid(reference)) {
+        // we should to convert the given reference to URL for downloading from Yarn
+        // package manager repository
+        return await(fetchPackage({
+            name,
+            reference: `https://registry.yarnpkg.com/${name}/-/${name}-${reference}.tgz`
+        }))
+    }
 
     let response = await fetch(reference);
 
